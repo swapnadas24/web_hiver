aa56c07caec760bbdd983a47cb6cb4abf5bc7b99
swapnadas24 <ksswapnadas@gmail.com>
2020-08-19 12:40:39 +0530

    first commit

---
+++ src/main/java/com/web/hiver/App.java
@@ -0,0 +1,13 @@
+package com.web.hiver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
